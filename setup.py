c275c4414e7fb1aac8cf9e26fd633514f01ff69f
Nathaniel Beckstead <becksteadn@gmail.com>
2018-11-29 22:43:36 -0500

    add entry point in setup

--- setup.py
+++ setup.py
@@ -18,4 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points = {'console_scripts': ['insight=insight.insight:main']},
 )
